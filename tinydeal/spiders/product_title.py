eec3e5ce7ab087a33aa65017237dbe2a4d52c4da
Nazmul Hasan <nazmulhasan747406@gmail.com>
2021-03-05 19:52:33 +0600

    added pagination

--- tinydeal/spiders/product_title.py
+++ tinydeal/spiders/product_title.py
@@ -4,7 +4,7 @@ import scrapy
 class ProductTitleSpider(scrapy.Spider):
     name = 'product_title'
     allowed_domains = ['www.tinydeals.co/product-category/smart-phones-tablets/']
-    start_urls = ['https://www.tinydeals.co/product-category/smart-phones-tablets/']
+    start_urls = ['https://www.tinydeals.co/product-category/smart-phones-tablets/%s' % x for x in range(13)]
 
     def parse(self, response):
         title = response.xpath("//div[@class='product-loop-header product-item__header']/a[@class='woocommerce-LoopProduct-link woocommerce-loop-product__link']/h2[@class='woocommerce-loop-product__title']")

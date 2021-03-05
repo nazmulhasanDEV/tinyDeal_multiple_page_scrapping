5fed23b87665e65e707366179363107384c66b1d
Nazmul Hasan <nazmulhasan747406@gmail.com>
2021-03-05 19:50:30 +0600

    added scrapping project and scraped tinydeal

---
+++ tinydeal/spiders/product_title.py
@@ -0,0 +1,15 @@
+import scrapy
+
+
+class ProductTitleSpider(scrapy.Spider):
+    name = 'product_title'
+    allowed_domains = ['www.tinydeals.co/product-category/smart-phones-tablets/']
+    start_urls = ['https://www.tinydeals.co/product-category/smart-phones-tablets/']
+
+    def parse(self, response):
+        title = response.xpath("//div[@class='product-loop-header product-item__header']/a[@class='woocommerce-LoopProduct-link woocommerce-loop-product__link']/h2[@class='woocommerce-loop-product__title']")
+
+        for x in title:
+            yield {
+                "Product Title" : x.xpath(".//text()").get()
+            }
